c5e42f58ad9d4963bd8b6db03181695617ff9316
BOSSNATY <natnaelweldemariam29@gmail.com>
2025-07-07 20:23:56 +0300

    update baseURL

--- Client/src/axisoConfig.js
+++ Client/src/axisoConfig.js
@@ -9,7 +9,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "https://evangadi-backend-jqcc.onrender.com",
+  baseURL: "https://evangadi-backend-jqcc.onrender.com/api",
 });
 
 axiosBase.interceptors.request.use((config) => {
